2f41ea044155387da693a74b9aec0efc5c1d25ab
Jeudy Robles <jeudyjr@outlook.com>
2024-01-07 18:50:27 -0600

    Se actualizo el forms modificando el html, css y javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,26 @@ window.onscroll = function() {
       document.querySelector('.up-page').style.display = 'none';
     }
 };
+
+//EMAILJS
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_amtzllg';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
